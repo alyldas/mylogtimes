ade33a5394cf99a1a97b08475067c4affed18f70
Andrey Markov <amarkov@undefined.team>
2021-10-28 21:25:51 +0300

    First try

---
+++ sw.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log("Service Worker really installed.");
+
+self.addEventListener("fetch", function (event) {
+  console.log("Service Worker really installed.");
+
+  var connection =
+    navigator.connection ||
+    navigator.mozConnection ||
+    navigator.webkitConnection;
+  var type = connection.type;
+
+  function updateConnectionStatus() {
+    console.log(
+      "Connection type changed from " + type + " to " + connection.type
+    );
+    type = connection.effectiveType;
+  }
+
+  connection.addEventListener("change", updateConnectionStatus);
+});
