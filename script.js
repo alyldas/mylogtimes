5321bac263453805e1887e3331354b5676a7082c
Andrey Markov <amarkov@undefined.team>
2021-10-28 23:32:18 +0300

    v4

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.write('v4');
+
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
         navigator.serviceWorker.register('./sw.js').then(function (registration) {
