144f2bbbd4ad501a60f5476d0102e4d1c904e291
Andrey Markov <amarkov@undefined.team>
2021-10-29 19:27:03 +0300

    Added loop to SW

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 self.addEventListener('activate', function (event) {
-    console.log('Hello from service worker!');
+    var i = 0;
+
+    function timedCount() {
+        i = i + 1;
+        console.log(i);
+        setTimeout("timedCount()", 500);
+    }
+
+    timedCount();
 });
