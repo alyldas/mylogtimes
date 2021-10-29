8d0c8467b311fb782aef944765f77ed7276047f9
Andrey Markov <amarkov@undefined.team>
2021-10-29 19:28:35 +0300

    SW loop second try

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
+var i = 0;
+
+function timedCount() {
+    i = i + 1;
+    console.log(i);
+    setTimeout("timedCount()", 500);
+}
+
 self.addEventListener('activate', function (event) {
-    var i = 0;
-
-    function timedCount() {
-        i = i + 1;
-        console.log(i);
-        setTimeout("timedCount()", 500);
-    }
-
     timedCount();
 });
